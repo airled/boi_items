2c487b3e2f0bce2711dc4e899159ed047b3a68f0
airled <airled@mail.ru>
2015-11-13 11:46:13 +0300

    imgs

--- source/handler.rb
+++ source/handler.rb
@@ -1,7 +1,6 @@
 def get_item(row)
   name = row.xpath('.//td[1]/a/@title').text
-  img_url = row.xpath('.//td[2]/a/img/@src').text
   desc = row.xpath('.//td[3]').text
-  {name: name, img_url: img_url, desc: desc}
+  {name: name, desc: desc}
 end
 
---
+++ source/img_fetcher.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+require 'curb'
+
+class Img_fetcher
+
+  URL = 'http://bindingofisaacrebirth.gamepedia.com'
+
+  def fetch
+    parse('/Items', '//div[@id="headertabs"]//a[@class="image"]/img')
+    parse('/Trinkets', '//table[@class="mw-collapsible wikitable striped trinkets"]//a[@class="image"]/img')
+  end
+  
+  private
+
+  def parse(url, path)
+    imgs = Nokogiri::HTML(Curl.get(URL + url).body).xpath(path)
+    imgs.map do |img|
+      img_name = img.xpath('./@alt').text.sub(' Icon','').gsub(/[ \/]/, '_').gsub("\'", '').strip
+      img_url = img.xpath('./@src').text.strip
+      unless File.exist?('./images/#{img_name}')
+        File.open("./images/#{img_name}", 'w') { |file| file << Curl.get(img_url).body }
+      end
+    end
+  end
+
+end
+
+Img_fetcher.new.fetch
