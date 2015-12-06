d012448991290ef1675eb50832b542563f993154
airled <airled@mail.ru>
2015-12-06 14:52:51 +0300

    new cookie

--- handler.rb
+++ handler.rb
@@ -23,7 +23,7 @@ end
 def get_html(url)
   c = Curl::Easy.new(url) do |request|
     # request.headers['Cookie'] = COOKIES
-    request.headers['Cookie'] = "__cfduid=d609c8aca747016636cac23c9f02608d41447261602; network_session=oz3kkykyhtB750j95xin2IFCDz45CDuCjcoPH0UWIoXZ3mHcPQUEjRI2TrArLGxo9v0RkE%2FuWJ2UyMKBWUI9ZMkVBStabPKb1coAcJnudPh9XWrVqlrSAtanHJkqHb7DB8%2BqooVCoLLg9PBo%2Bso%2BeaL5fv3pXUclzY0279y0gcA%3D%7CVLRE%2B4cAv6QESVpFPE8FeGgp7ARHnSvHatLYTKuzVpI%3D; cdmu=1; cdmua=1447261611147; __utma=23258028.821811448.1447261613.1447782595.1447790525.5; __utmz=23258028.1447790525.5.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); cdmblk=0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0; __utmb=23258028.12.9.1447790810049; __utmc=23258028; __utmt=1; __utmt_b=1; __utmt_c=1; bknx_fa=1447790525903; bknx_ss=1447790525903"
+    request.headers['Cookie'] = "__cfduid=d81b65191ca875daa10015332411434a81449400361; network_session=P1ZWIh0xCl2vqOyBhxZy%2FND0ayJibcy1owBg5aY%2FzL5SWej5toToRmb%2B%2FqFUPmBMknCljKqCqvn8MTJxnEf1e6e27tx6brtxTzAYHURfS9rfyg7MT1s6R0EBM4PcWNMx9y8wRP4Mldj1hUTHFXkXVMboHjsT250mgw7TSInDEpg%3D%7CpzGQWCNOFGqTip1fA2iiLf2ttJ7wJs9XVkYM6Rv1aXs%3D; cdmua=1449400388245; cdmblk=0:0:0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0:0:0,0:0:0:0:0:0:0:0:0:0:0; __utma=23258028.986341314.1449400396.1449400396.1449400396.1; __utmc=23258028; __utmz=23258028.1449400396.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); bknx_fa=1449400397790; bknx_ss=1449400397790; cdmtlk=6403:6403:6403:6403:6403:100:6403:6403; cdmgeo=by; cdmbaserate=2.5; cdmbaseraterow=1.5"
   end
   c.perform
   Nokogiri::HTML(c.body)
