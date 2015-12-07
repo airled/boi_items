6fc6fe621f8c9a457ed4b1067e82cdcf2d505606
airled <airled@mail.ru>
2015-12-07 08:11:22 +0300

    update 07.12.2015

--- cookies_fetcher.rb
+++ cookies_fetcher.rb
@@ -25,6 +25,24 @@ class Cookies_fetcher
     puts 'Cookies got.'
   end
 
+  # def do_it(url, redirect_count, cookies)
+  #   c = Curl::Easy.new(url) do |req|
+  #     req.headers["User-Agent"] = AGENT
+  #     req.headers["Cookie"] = cookies if cookies
+  #   end
+  #   c.perform
+  #   redirect_url = header_value(c.head, 'Location')
+  #   got_cookies = header_value(c.head, 'Set-Cookie').empty? ? nil : header_value(c.head, 'Set-Cookie')
+  #   puts redirect_url
+  #   puts got_cookies
+  #   puts
+  #   if redirect_count == 4
+  #     return {redirect_url: redirect_url, got_cookies: got_cookies}
+  #   else
+  #     do_it(redirect_url, redirect_count + 1, got_cookies)
+  #   end
+  # end
+
   private
 
   def request(url, cookies = nil)
@@ -43,3 +61,5 @@ class Cookies_fetcher
   end
   
 end
+
+# do_it(URL, 1, nil)
